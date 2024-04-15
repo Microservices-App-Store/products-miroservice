da203901b9cea4a6900c05068d2b802ebd2de89b
sotacode <sotacodeprivate@gmail.com>
2024-04-16 08:59:16 +1200

    products-service done@

---
+++ src/products/products.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger(ProductsService.name);
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected to the database');
+  }
+  create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto
+    });
+  }
+
+  async findAll(paginatioDto: PaginationDto) {
+    const { page, limit } = paginatioDto;
+    const total = await this.product.count({ where: { available: true }});
+    const totalPages = Math.ceil(total / limit);
+    return {
+      data: await this.product.findMany({
+        take: limit,
+        skip: (page - 1) * limit,
+        where: { available: true }
+      }),
+      meta: {
+        page,
+        totalPages,
+        total
+      }
+    }
+  }
+
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id, available: true}
+    });
+
+    if(!product) throw new NotFoundException(`Product with id ${id} not found`);
+
+    return product
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    await this.findOne(id);
+    const { id: _, ...data } = updateProductDto;
+    const product = this.product.update({
+      where: { id },
+      data: data
+    });
+
+    return product;
+  }
+  
+
+  async remove(id: number) {
+    await this.findOne(id);
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false }
+    }
+    );
+
+    return product
+  }
+}
